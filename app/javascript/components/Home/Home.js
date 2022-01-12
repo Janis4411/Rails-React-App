014109a9c5540cb5fc122a1bd8ae20a876e45821
Janis4411 <janis.vaneylen@gmail.com>
2022-01-12 13:00:06 +0100

    set up new rails react app

---
+++ app/javascript/components/Home/Home.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import Jumbotron from './Jumbotron'
+import Table from './Table/Table'
+
+
+class Home extends Component {
+   constructor() {
+     super()
+
+     this.state = {
+       course_modules: [
+         { id: 1, title: '1. Setting up a new Ruby on Rails App with React.', description: 'lorem Ipsum', active: false },
+         { id: 2, title: '2. Adding React to an existing Rails App.', description: 'lorem Ipsum', active: false },
+         { id: 3, title: '3. Building a Hello World App.', description: 'lorem Ipsum', active: false },
+         { id: 4, title: '4. Adding React Router Dom to your App.', description: 'lorem Ipsum', active: false },
+       ]
+     }
+   }
+
+  render() {
+    return (
+        <div>
+          <Jumbotron/>
+          <Table course_modules={this.state.course_modules}/>
+        </div>
+      )
+  }
+}
+
+export default Home
