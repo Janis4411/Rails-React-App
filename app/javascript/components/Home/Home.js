6cba3c93dd3286be7296d8f30070565d6e5b944f
Janis4411 <janis.vaneylen@gmail.com>
2022-01-12 23:51:01 +0100

    rails react app setup

--- app/javascript/components/Home/Home.js
+++ app/javascript/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Jumbotron from './Jumbotron'
 import Table from './Table/Table'
+import axios from 'axios'
 
 
 class Home extends Component {
@@ -8,20 +9,45 @@ class Home extends Component {
      super()
 
      this.state = {
-       course_modules: [
-         { id: 1, title: '1. Setting up a new Ruby on Rails App with React.', description: 'lorem Ipsum', active: false },
-         { id: 2, title: '2. Adding React to an existing Rails App.', description: 'lorem Ipsum', active: false },
-         { id: 3, title: '3. Building a Hello World App.', description: 'lorem Ipsum', active: false },
-         { id: 4, title: '4. Adding React Router Dom to your App.', description: 'lorem Ipsum', active: false },
-       ]
+       course_modules: []
      }
+   }
+
+   componentDidMount(){
+     axios.get('/episodes.json')
+     .then(data => {
+       let res = []
+       data.data.data.map( (data) => {
+         res.push({id: data.id, title: data.title, description: data.description, active: false})
+
+         this.setState({course_modules: res})
+       })
+     })
+     .catch(data => {
+
+     })
+   }
+
+  handleVideoChange(item, event) {
+    event.preventDefault()
+      let course_modules = [...this.state.course_modules]
+
+      course_modules.map( data => {
+        data.active = false
+      })
+
+      item.active = !item.active
+
+      course_modules[item.id - 1] = item
+
+      this.setState({course_modules})
    }
 
   render() {
     return (
         <div>
           <Jumbotron/>
-          <Table course_modules={this.state.course_modules}/>
+          <Table handleVideoChange={this.handleVideoChange.bind(this)} course_modules={this.state.course_modules}/>
         </div>
       )
   }
--- app/javascript/components/Home/Table/Item.js
+++ app/javascript/components/Home/Table/Item.js
@@ -1,5 +1,20 @@
 import React, { Component } from 'react'
 import Thumbnail from './Thumbnail'
+import styled from 'styled-components'
+
+const Button = styled.a`
+  display: inline-block;
+  text-decoration: none;
+  font-weight: bold;
+  cursor: pointer;
+  border-radius: 0;
+  background: #fff;
+  padding: 10px 20px;
+  font-size: 18px;
+  box-shadow: 0px 0px 0px 3px #473228,
+    -6px 6px #ef5f17,
+    -6px 6px 0px 3px #473228;
+`
 
 
 const Item = (props) => {
@@ -17,7 +32,7 @@ const Item = (props) => {
                   <h4>{props.title}</h4>
                   <p>{props.description}</p>
                     <div className="cta-wrapper">
-                      <a className="btn cta-btn">Watch This Video</a>
+                      <Button onClick={props.handleVideoChange} className="btn cta-btn">Watch This Video</Button>
                     </div>
                 </div>
               </div>
